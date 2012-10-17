5311de3b619f810f27fdfe3e2edb41f0809a8832
Vitor Leal <vitorleal1@gmail.com>
2012-10-18 00:19:37 +0200

    refactor in the maps functions and added the google maps api key

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,22 +1,58 @@
-function initialize() {
-  var latlng = new google.maps.LatLng(-34.397, 150.644),
-  myOptions  = {
-    zoom: 8,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+var maps = {
+
+  map: null,
+  center: {
+    lat: 40.416691,
+    lng: -3.700345
   },
-  map = new google.maps.Map(document.getElementById("map"), myOptions);
+  zoom: 13,
+
+  init: function(lat, lng) {
+    var latlng = new google.maps.LatLng(lat, lng),
+        mapOptions  = {
+          zoom: this.zoom,
+          center: latlng,
+          disableDefaultUI: true,
+          zoomControl: true,
+          zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+            position: google.maps.ControlPosition.TOP_RIGHT
+          },
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+    this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    this.setMarker(latlng, this.map);
+  },
+
+  setMarker: function(latlng, map) {
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map,
+        title: 'Hello World!',
+        animation: google.maps.Animation.DROP
+    });
+  },
+
+  setMarkers: function(markers, map) {
+
+  },
+
+  resize: function() {
+    var w = $(window).width(),
+        h = $(window).height();
+
+    $('#map').width(w).height(h);
+  }
 }
 
-function resizeMap() {
-	$('#map').width($(window).width()).height($(window).height());
-}
-
+//init map
 $(function () {
-	resizeMap();
-	initialize();
+  maps.resize();
+	maps.init(maps.center.lat, maps.center.lng);
 });
 
+//resize map
 $(window).resize(function () {
-	resizeMap();
+	maps.resize();
 });
