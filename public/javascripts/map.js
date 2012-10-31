b1ed1d512e184469dd2e295912a1b3ddce2389fa
Vitor Leal <vitorleal1@gmail.com>
2012-10-31 02:32:17 +0100

    added other etinerary

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -155,6 +155,160 @@ var maps = {
     ]
   ],
 
+  days2: [
+    [
+      {
+        "title": "Hotel Victoria",
+        "description": "This historic hotel is in the heart of Madrid, by the Puerta del Sol. Facilities include free Wi-Fi and a café-bar on the large outdoor terrace. The Hotel Victoria 4 dates back to 1850 and offers elegant, renovated interiors. Rooms at Victoria 4 are soundproofed, with a free safety deposit box. They all offer a spacious bathroom with a bathtub and hairdryer. Continental breakfast is included in rates. There is a traditional restaurant and a range of tapas are on offer at the Café del Levante. Sol Metro Station is metres away and offers easy transport around the city. Victoria 4 is in a lively area, within walking distance of Gran Vía and the Art Triangle.",
+        "geo": {
+          "lat": "40.416204",
+          "lng": "-3.701728"
+        },
+        "address": "Victoria, 4, Centro, 28012 Madrid"
+      },
+      {
+        "title": "El Parque del Retiro",
+        "description": "This park is known as the lungs of Madrid, and few cities can boast such a large park in such a central location. There's a large boating lake and three art galleries (Casa de Vacas, Palacio de Cristal and Palacio de Velázquez). On weekends and holidays, it fills with madrileños (citizens of Madrid) and performers of all sorts, including mime artists and jugglers, painters, singers, puppet masters and fortune-tellers. It was built as a royal park in the 17th Century, and until 1868 it was the exclusive reserve of the aristocracy.",
+        "geo": {
+          "lat": "40.425911452005714",
+          "lng": "-3.689088821411133"
+        },
+        "address": "Calle de Alfonso XII, 14, 28014 Madrid, Spain"
+      },
+      {
+        "title": "Sama Samá Restaurant",
+        "description": "In the center of Madrid, Chueca. Sama Samá is one of the most tempting gastronomic options in Madrid. It is an intimate space where your lighting helps make it a warm and cozy, decorated in the style of French romantic. Taste innovative creative cuisine surprises diners each for its sophistication and originality. Within its varied menu you can find dishes like duck confit croquettes with fig jam, roasted Iberian sirloin with caramelized onions and red wine sauce and truffled risotto with mushrooms and balsamic, are able to satisfy the most demanding palates. If you are a lover of good food in an exceptional setting, Sama Sama is the proper bet.",
+        "geo": {
+          "lat": "40.42623814217773",
+          "lng": "-3.69840145111084"
+        },
+        "address": "Calle de San Bartolomé, 23, 28004 Madrid, Spain +34 915 21 55 47"
+      },
+      {
+        "title": "Chueca Neighborhood (Walking and Drinks) ",
+        "description": "Chueca in Madrid is the gay area and home to the coolest bars and restaurants around. The main street is packed with bars; the most difficult thing is to choose where to go. It makes for a very good bar crawl, but don’t go there if you are homophobic as it is very overtly gay.  A good starting point is Plaza de Chueca as the metro is there and most of the good streets for going out are around there. Try Sierra Angel, a little, old vermouth bar with lots of character which overlooks the square to get you going. Remember in Madrid nothing gets going until around 10 or later so have a siesta and eat late so you are ready to party into the early hours with the Spaniards.",
+        "geo": {
+          "lat": "40.455829687131434",
+          "lng": "-3.6975860595703125"
+        },
+        "address": "Chueca, Madrid, Spain"
+      }
+    ],
+
+    [
+      {
+        "title": "Shopping Calle Serrano",
+        "description": "Famous street for shopping where you will find the most exclusive brands and restaurantes to spend some time",
+        "geo": {
+          "lat": "40.44609792343051",
+          "lng": "-3.686342239379883"
+        },
+        "address": "Calle Serrano, Madrid, Spain"
+      },
+       {
+        "title": "Puerta de Alcalá",
+        "description": "One of Madrid's enduring symbols, it's made entirely of granite with one central arch and two smaller passageways on each side. Carlos III had his Court architect Sabatini build the gate in 1778 as a monument to himself and to show visiting merchants from Aragon how important the city had become. The ornamental statues are the work of Roberto Michel and Francisco Gutierrez. Its location is spectacular, as it stands on raised ground opposite another symbol of the city, Cibeles Fountain",
+        "geo": {
+          "lat": "40.42243210323737",
+          "lng": "-3.6884236335754395"
+        },
+        "address": "Puerta de Alcalá, Madrid, Spain"
+      },
+       {
+        "title": "Fuente de Cibeles",
+        "description": "Another majestic work of art commissioned by King Carlos III to ennoble the city. This time his architects chose to create an image in white stone of the goddess Cibeles (La Fuente de Cibeles) driving a carriage drawn by lions. Ventura Rodríguez came up with the design while Roberto Michel and Francisco Gutierrez carried out the work. It's surrounded by some of the city's most important historic buildings and it has become a favorite symbolic landmark for madrileños. The fans of Real Madrid football club gather here to celebrate their team's victories.",
+        "geo": {
+          "lat": "40.42761022357258",
+          "lng": "-3.6931228637695312"
+        },
+        "address": "Plaza de Cibeles, 28014 Madrid, Spain"
+      },
+      {
+        "title": "Museo del Prado",
+        "description": "Considered to be an important landmark in Madrid's rich art history, the Museo Nacional del Prado exhibits paintings by Goya, Velázquez, Murillo, Zurbarán, El Greco, Ribera, Titian, Boticelli, Tintoretto, El Bosco, Rubens, Van Dyck, Poussin and Durer. Carlos III commissioned Juan de Villanueva to design this beautiful building, which was converted into an art gallery in 1819. In July of 2005, the Council of Ministers approved an expansion plan which increased the museum's total space by 50 percent, and allowed for 500 more works of the permanent collection to be showcased. On October 30, 2007, the annex opened with a collection of 19th-century Spanish artwork which enabled the Prado to reclaim its glory as a crown jewel of Europe. Come for the art; stay for the architecture.",
+        "geo": {
+          "lat": "40.413777",
+          "lng": "-3.6924679999999626"
+        },
+        "address": "Paseo del Prado, Madrid"
+      },
+      {
+        "title": "Museo Nacional Centro de Arte Reina Sofía, Madrid",
+        "description": "This gallery of modern art features work by some of the 20th Century's most outstanding artists, including Picasso - Guernica has its own special space - Dalí, Tàpies, Chillida, Miró, Gris, Bacon, Le Cobursier, Lipchitz, Schnabel, Numan, and Roberto Matta. There is a library, research center, bookshop, cafe, souvenir shop, and restaurant.",
+        "geo": {
+          "lat": "40.42042281986496",
+          "lng": "-3.6673736572265625"
+        },
+        "address": "Calle de Santa Isabel, 52, 28012 Madrid, Spain"
+      },
+      {
+        "title": "La Puerta del Sol",
+        "description": "Sun Gate (Puerta del Sol) is the most popular and commercial area in the old town. Five hundred years ago it was just another city gate with an image of the sun drawn on it, hence its name. Nowadays, the square is famous for being the center of the national road network. The plaque on the pavement outside the old Post Office building denotes 'kilometer 0'. The distance between Madrid and every other part of Spain is measured from here. Thousands of revelers gather in the square once a year to await the chimes of the clock that officially announces the New Year. Open 24 hours",
+        "geo": {
+          "lat": "40.416876",
+          "lng": "-3.703305"
+        },
+        "address": "C. Mayor / C. Alcala, 28013 Madrid, 28013, Madrid, Spain"
+      },
+      {
+        "title": "Plaza Mayor",
+        "description": "There are many ways to spend your time and money at this attractive central square: stroll around and admire the architecture and the murals or even buy an original watercolor from one of the many pavement artists. During the Middle Ages, merchants congregated here to live and sell their wares. When Felipe II established the Court in Madrid, he decided that this square should be reserved for special occasions, and it started to take its present shape from 1790 onwards. The statue in the center is of Felipe III.",
+        "geo": {
+          "lat": "40.42408195771912",
+          "lng": "-3.7067699432373047"
+        },
+        "address": "Plaza Mayor, 30 28012 Madrid, Spain"
+      }
+    ],
+
+    [
+      {
+        "title": "Hotel Victoria",
+        "description": "This historic hotel is in the heart of Madrid, by the Puerta del Sol. Facilities include free Wi-Fi and a café-bar on the large outdoor terrace. The Hotel Victoria 4 dates back to 1850 and offers elegant, renovated interiors. Rooms at Victoria 4 are soundproofed, with a free safety deposit box. They all offer a spacious bathroom with a bathtub and hairdryer. Continental breakfast is included in rates. There is a traditional restaurant and a range of tapas are on offer at the Café del Levante. Sol Metro Station is metres away and offers easy transport around the city. Victoria 4 is in a lively area, within walking distance of Gran Vía and the Art Triangle.",
+        "geo": {
+          "lat": "40.416204",
+          "lng": "-3.701728"
+        },
+        "address": "Victoria, 4, Centro, 28012 Madrid"
+      },
+      {
+        "title": "Calle Gran Vía",
+        "description": "Is an ornate and upscale shopping street located in central Madrid. It leads from Calle de Alcalá, close to Plaza de Cibeles, to Plaza de España. The lively street is one of the city's most important shopping areas, with a large number of hotels and large movie theaters; it is also noted for the grand architecture prevalent among many of its buildings. Now, most of the theaters are being replaced by shopping malls. It is considered a showcase of early 20th century architecture, with patterns ranging from Vienna Secession style, Plateresque, Neo-Mudéjar, Art Deco and others",
+        "geo": {
+          "lat": "40.42421263753269",
+          "lng": "-3.703465461730957"
+        },
+        "address": "Calle Gran Vía, Madrid, Spain"
+      },
+      {
+        "title": "Almudena Cathedral",
+        "description": "Madrid's cathedral took over 100 years to build, due to political conflicts, wars, and a lack of financial resources. According to legend, it's named after the Virgen de la Almudena, whose icon was found during the Reconquest hidden in a house that the Arabs called al mudena. The building's exterior is an example of eclectic 19th-century design. The doors are bronze and there's an image of the Virgen (now Madrid's patron saint) on the façade. The interior is full of light, benefiting from the stained-glass windows and multi-colored ceiling. Its grand Grezing organ gets played on Sundays, holidays, and the day before holidays.",
+        "geo": {
+          "lat": "40.424343317092344",
+          "lng": "-3.7142372131347656"
+        },
+        "address": "Calle de Bailén, Madrid 28013"
+      },
+      {
+        "title": "Royal Palace of Madrid",
+        "description": "This former royal residence (Palacio Real) houses a fantastic collection of paintings (Velázquez, Caravaggio and Goya), sculptures, furniture, tapestries, glassware, gold and silverware, medals, porcelain and musical instruments. It is well worth visiting the rooms decorated by Gasparini as well as the Royal Pharmacy, the Library, and the Royal Armory. King Felipe V commissioned the Italian architect Sabatini to design the palace, and the work was carried out between 1738-1770.",
+        "geo": {
+          "lat": "40.417955",
+          "lng": "-3.7143120000000636"
+        },
+        "address": "Calle de Bailén, Madrid"
+      },
+      {
+        "title": "Toledo Half Day Tour",
+        "description": "Visit the imperial city of Toledo, one of the oldest cities in Europe and home to works of El Greco's art, on this half-day tour from Madrid. See the Church of Santo Tomé, the Monastery of San Juan de los Reyes, Santa María la Blanca Synagogue, and more! Highlights: Travel to the imperial city of Toledo, one of the oldest cities in Europe Take a panoramic tour of Toledo and learn more about its rich history Admire the works of El Greco, including his masterpiece El Entierro del senor de Orgaz View the Gothic Cathedral and its stunning architecture from outside See Santa María la Blanca Synagogue, one of the oldest in Europe Visit the Monastery of San Juan de los Reyes Learn about the famous technique of Damasquinado, the art of metal inlay", "geo": {
+          "lat": "40.42127228032075",
+          "lng": "-3.711576461791992"
+        },
+        "address": "Plaza de Oriente 8, 28013 Madrid, Spain"
+      }
+    ]
+  ],
+
   map: null,
   directionsDisplay: null,
   directionsService: new google.maps.DirectionsService(),
@@ -299,7 +453,13 @@ var maps = {
 
 
   showDay: function (num) {
-    var pointsDays = maps.days[num];
+    var pointsDays;
+
+    if ($('.type-of-trip').text() === "Romantic") {
+      pointsDays = maps.days2[num];
+    } else {
+      pointsDays = maps.days[num];
+    }
 
     this.clearMap();
     this.setMarkers(pointsDays);
